0acd430a5c7908f96a02e5341bcbf3857cd51256
nial <aditya.suryavamshi@razorpay.com>
2021-03-26 21:39:05 +0530

    Adding problem statement for day 5

--- daily_coding_problems/day5.go
+++ daily_coding_problems/day5.go
@@ -1,5 +1,18 @@
 package puzzles
 
+/*
+This problem was asked by Jane Street.
+
+cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+
+Given this implementation of cons:
+
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
+Implement car and cdr.
+*/
 func cons(a, b int) func(func(int, int) int) int {
 	pair := func(f func(a, b int) int) int {
 		return f(a, b)
